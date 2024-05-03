c8d6a8f1b794a8f8f1ef549399d0abffc3bf8477
cisjacob <francistaino16@gmail.com>
2024-05-03 17:25:19 +0800

    Updates.

---
+++ src/components/Nav.tsx
@@ -0,0 +1,13 @@
+const Nav = () => {
+  return (
+    <header>
+      <nav>
+        <a href="/">
+          <img src={headerLogo} />
+        </a>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
