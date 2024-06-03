67a1aca3be6216978b98d5cbc39442e8009b1624
cisjacob <francistaino16@gmail.com>
2024-06-03 17:26:44 +0800

    Updates

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
       id="home"
       className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
-      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28 border-2">
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
         <p className="text-xl font-montserrat text-coral-red">
           Our Summer Collection
         </p>
@@ -41,7 +41,7 @@ const Hero = () => {
           ))}
         </div>
       </div>
-      <div className="border-2 border-red-500 relative flex flex-1 justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+      <div className="relative flex flex-1 justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
         <img
           src={bigShoeImg}
           alt="shoe collection"
